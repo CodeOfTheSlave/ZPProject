08eafa60bbb75e37a0dfd7311d96722652493a90
lizp <13699836954@139.com>
2016-08-07 14:30:48 +0800

    core data 多表关联   by lizp

--- ZPProject/First/coreData/Person+CoreDataProperties.h
+++ ZPProject/First/coreData/Person+CoreDataProperties.h
@@ -2,8 +2,8 @@
 //  Person+CoreDataProperties.h
 //  ZPProject
 //
-//  Created by lizp on 16/5/4.
-//  Copyright © 2016年 Apple05. All rights reserved.
+//  Created by lizp on 16/8/7.
+//  Copyright © 2016年 LZP. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -15,9 +15,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Person (CoreDataProperties)
 
-@property (nullable, nonatomic, retain) NSString *name;
 @property (nullable, nonatomic, retain) NSNumber *age;
-@property (nullable, nonatomic, retain) Department *department;
+@property (nullable, nonatomic, retain) NSString *name;
+@property (nullable, nonatomic, retain) Department *relationship;
 
 @end
 
--- ZPProject/First/coreData/Person+CoreDataProperties.m
+++ ZPProject/First/coreData/Person+CoreDataProperties.m
@@ -2,8 +2,8 @@
 //  Person+CoreDataProperties.m
 //  ZPProject
 //
-//  Created by lizp on 16/5/4.
-//  Copyright © 2016年 Apple05. All rights reserved.
+//  Created by lizp on 16/8/7.
+//  Copyright © 2016年 LZP. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -13,8 +13,8 @@
 
 @implementation Person (CoreDataProperties)
 
-@dynamic name;
 @dynamic age;
-@dynamic department;
+@dynamic name;
+@dynamic relationship;
 
 @end
