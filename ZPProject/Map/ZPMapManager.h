00e475b85eca9e4ceab52ae51651900119e8f77d
lizp <lizp@gyist.com>
2016-08-11 18:16:10 +0800

    百度地图  by lizp

--- ZPProject/Map/ZPMapManager.h
+++ ZPProject/Map/ZPMapManager.h
@@ -13,8 +13,20 @@
 +(ZPMapManager *)shareInstance;
 
 /**
- *  启动引擎
+ *  启动百度地图引擎
  */
 -(void)start;
+
+
+/**
+ *  检查定位服务是否打开
+ *
+ *  @return YES 可用  NO 不可用
+ */
+-(BOOL)cheakLocationServer;
+
+
+
+
 
 @end
--- ZPProject/Map/ZPMapManager.m
+++ ZPProject/Map/ZPMapManager.m
@@ -8,6 +8,7 @@
 
 #import "ZPMapManager.h"
 #import "BMKMapManager.h"
+#import <CoreLocation/CoreLocation.h>
 
 @interface ZPMapManager()<BMKGeneralDelegate>
 
@@ -24,21 +25,46 @@
     return manager;
 }
 
+
+
 -(void)start {
     
     NSString *mapKey = @"FEAUcrfePf7MbyisTClGlyVa2ugP5l9D";
-    BOOL isStart = [[BMKMapManager alloc] start:mapKey generalDelegate:self];
+    BMKMapManager *manager = [[BMKMapManager alloc] init];
+    BOOL isStart =[manager start:mapKey generalDelegate:self];
     NSLog(@"%d",isStart);
+    
 }
 
+-(BOOL)cheakLocationServer {
+
+    BOOL isServer = [CLLocationManager locationServicesEnabled];
+    return isServer;
+}
+
+#pragma private
+
+#pragma mark- 定位服务是否开启
+-(BOOL)locationServiceEnabled {
+    return [CLLocationManager locationServicesEnabled];
+}
 
 #pragma mark- BMKGeneralDelegate
 -(void)onGetNetworkState:(int)iError {
-    NSLog(@"onGetNetworkState:%d",iError);
+    if(iError == 0) {
+        NSLog(@"connext to network is success:%d",iError);
+    }else {
+        NSLog(@"connext to network is fail:%d",iError);
+    }
+    
 }
 
 -(void)onGetPermissionState:(int)iError {
-     NSLog(@"onGetPermissionState:%d",iError);
+    if(iError == 0) {
+        NSLog(@"start baiduMap service success:%d",iError);
+    }else {
+         NSLog(@"start baiduMap service fail:%d",iError);
+    }
 }
 
 @end
